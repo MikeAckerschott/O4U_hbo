1f85f53e635e715c38019352dfbc79e6a92627de
Wouter <wj.molemaker@student.han.nl>
2024-10-03 16:06:25 +0200

    projectview update

--- front-end/uncenshow/src/views/Projectview.vue
+++ front-end/uncenshow/src/views/Projectview.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="container mt-4">
-    <table class="table table-hover">
-      <thead>
+    <table class="table table-hover table-striped">
+      <thead class="table-dark">
         <tr>
-          <th scope="col">Following</th>
-          <th scope="col">Name</th>
-          <th scope="col">ID</th>
+          <th scope="col" @click="sortTable('following')">
+            Following
+            <i :class="getSortIcon('following')"></i>
+          </th>
+          <th scope="col" @click="sortTable('name')">
+            Name
+            <i :class="getSortIcon('name')"></i>
+          </th>
+          <th scope="col" @click="sortTable('id')">
+            ID
+            <i :class="getSortIcon('id')"></i>
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in items" :key="item.id" @click="navigateToDetail(item.id)">
+        <tr v-for="item in sortedItems" :key="item.id" @click="navigateToDetail(item.id)">
           <td>{{ item.following }}</td>
           <td>{{ item.name }}</td>
           <td>{{ item.id }}</td>
@@ -30,9 +39,34 @@ export default {
         { following: 'Yes', name: 'Alice Johnson', id: 3 },
         // Add more items as needed
       ],
+      sortKey: '',
+      sortOrder: 1,
     };
   },
+  computed: {
+    sortedItems() {
+      return this.items.sort((a, b) => {
+        if (a[this.sortKey] < b[this.sortKey]) return -1 * this.sortOrder;
+        if (a[this.sortKey] > b[this.sortKey]) return 1 * this.sortOrder;
+        return 0;
+      });
+    },
+  },
   methods: {
+    sortTable(key) {
+      if (this.sortKey === key) {
+        this.sortOrder *= -1;
+      } else {
+        this.sortKey = key;
+        this.sortOrder = 1;
+      }
+    },
+    getSortIcon(key) {
+      if (this.sortKey === key) {
+        return this.sortOrder === 1 ? 'bi bi-arrow-up' : 'bi bi-arrow-down';
+      }
+      return 'bi bi-arrow-down-up';
+    },
     navigateToDetail(id) {
       this.$router.push({ name: 'DetailPage', params: { id } });
     },
@@ -42,6 +76,9 @@ export default {
 
 <style>
 .table-hover tbody tr:hover {
+  cursor: pointer;
+}
+th {
   cursor: pointer;
 }
 </style>
