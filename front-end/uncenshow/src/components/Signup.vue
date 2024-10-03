7e88a8162d1292d2a56351692344ebfe30e393eb
Wouter <wj.molemaker@student.han.nl>
2024-10-03 15:34:23 +0200

    frontend

---
+++ front-end/uncenshow/src/components/Signup.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="signup center">
+    <div class="container">
+      <ErrorMessage
+        v-if="store.getters.getErrorMessage != null"
+        :message="store.getters.getErrorMessage"
+      ></ErrorMessage>
+      <form class="form">
+        <TextBox
+          :emitUpdate="handleUpdatedUsername"
+          placeholder="Username"
+          :value="username"
+          class_name="textbox"
+          type="username"
+          label="Signup"
+        />
+        <p v-if="!isMinLengthUsername() && username.length != 0">
+          The username should be atleast {{ usernameLength.min }} characters long
+        </p>
+        <p v-if="!isMaxLengthUsername()">
+          The username should be atmost {{ usernameLength.max }} characters long
+        </p>
+        <TextBox
+          :emitUpdate="handleUpdatedPassword"
+          placeholder="Password"
+          :value="password"
+          class_name="textbox"
+          type="password"
+          label="Password"
+        />
+        <p v-if="!isMinLengthPassword() && password.length != 0">
+          The password should be atleast {{ passwordLength.min }} characters long
+        </p>
+        <p v-if="!isMaxLengthPassword()">
+          The password should be atmost {{ passwordLength.max }} characters long
+        </p>
+        <p v-if="!isPasswordContainingUppercase() && password.length != 0">
+          The password should contain atleast one uppercase letter
+        </p>
+        <p v-if="!isPasswordContainingLowercase() && password.length != 0">
+          The password should contain atleast one lowercase letter
+        </p>
+        <p v-if="!isPasswordContainingSymbol() && password.length != 0">
+          The password should contain atleast one symbol
+        </p>
+        <p v-if="!isPasswordContainingNumber() && password.length != 0">
+          The password should contain atleast one number
+        </p>
+
+        <label class="label" for="image">Image</label>
+
+        <p v-if="!isValidImage() && image !== null" class="error-Text">
+          Invalid image. Please choose a valid image file. <br/>
+          The image should be less than 1MB in size,
+          And should be of type jpeg, png or gif.
+        </p>
+
+
+        <input class="input" id="image" type="file" accept="image/*" @change="handleImageChange" />
+
+        <button
+          :class="{
+            button: isValidPassword() && isValidUsername(),
+            buttonDisabled: !isValidPassword() || !isValidUsername()
+          }"
+          @click.prevent="isValidPassword() && isValidUsername()? signup() : null"
+        >
+          Signup
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import TextBox from './TextBox.vue'
+import ErrorMessage from './ErrorMessage.vue'
+
+export default defineComponent({
+  name: 'SignupPage',
+  components: {
+    TextBox,
+    ErrorMessage
+  },
+  data() {
+    return {
+      store: useStore(),
+      username: '',
+      password: '',
+      image: null as File | null,
+      usernameLength: {
+        min: 4,
+        max: 50
+      },
+      passwordLength: {
+        min: 10,
+        max: 50
+      }
+    }
+  },
+  methods: {
+    async signup() {
+
+      await this.store.dispatch('signup', {
+        username: this.username,
+        password: this.password,
+        image: this.image
+      })
+
+      const user = this.store.getters.getUser
+      if (user) {
+        this.$router.push({ name: 'home' })
+      }
+    },
+    handleImageChange(event: Event) {
+      const target = event.target as HTMLInputElement
+      if (target.files && target.files.length > 0) {
+        this.image = target.files[0]
+      }
+    },
+    isValidImage() {
+      if (this.image === null) {
+        return false
+      }
+
+      if (this.image.size > 1000000) {
+        return false
+      }
+
+      return this.image !== null && ['image/jpeg', 'image/png', 'image/gif'].includes(this.image.type);
+    },
+
+    handleUpdatedUsername(value: string) {
+      this.username = value
+    },
+
+    isValidUsername() {
+      return (
+        this.username.length >= this.usernameLength.min &&
+        this.username.length <= this.usernameLength.max
+      )
+    },
+
+    isMinLengthUsername() {
+      return this.username.length >= this.usernameLength.min
+    },
+
+    isMaxLengthUsername() {
+      return this.username.length <= this.usernameLength.max
+    },
+
+    isMinLengthPassword() {
+      return this.password.length >= this.passwordLength.min
+    },
+
+    isMaxLengthPassword() {
+      return this.password.length <= this.passwordLength.max
+    },
+
+    isPasswordContainingUppercase() {
+      const uppercaseRegex = new RegExp('^(?=.*[A-Z])')
+      return uppercaseRegex.test(this.password)
+    },
+
+    isPasswordContainingLowercase() {
+      const lowercaseRegex = new RegExp('^(?=.*[a-z])')
+      return lowercaseRegex.test(this.password)
+    },
+
+    isPasswordContainingSymbol() {
+      const symbolRegex = new RegExp('^(?=.*[!@#\$%\^&\*])')
+      return symbolRegex.test(this.password)
+    },
+
+    isPasswordContainingNumber() {
+      const numberRegex = new RegExp('^(?=.*[0-9])')
+      return numberRegex.test(this.password)
+    },
+
+    isValidPassword() {
+      const mediumRegex = new RegExp(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@$!%*?&]{8,50}\$'
+      )
+      return mediumRegex.test(this.password)
+    },
+
+    handleUpdatedPassword(value: string) {
+      this.password = value
+    }
+  }
+})
+</script>
+
+<style scoped>
+.flexbox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.container {
+  width: 400px;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 20px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+.label {
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+.input {
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+}
+.button {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #3f51b5;
+  color: white;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.buttonDisabled {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #3b383f;
+  color: rgb(146, 146, 146);
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.error-Text {
+  color: rgb(181, 181, 181);
+  font-size: 14px;
+  padding-bottom: 10px;
+  line-height: 1.2;
+}
+</style>
---
+++ front-end/uncenshow/src/components/VueSidePanel.vue
@@ -0,0 +1,484 @@
+<script lang="ts">
+import {
+  defineComponent,
+  onBeforeMount,
+  onBeforeUnmount,
+  type PropType,
+  watch,
+  ref,
+  computed,
+  onMounted,
+  nextTick
+} from 'vue'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import SidePanelCloseButton from './SidePanelCloseButton.vue'
+
+export default defineComponent({
+  name: 'VueSidePanel',
+  components: {
+    SidePanelCloseButton
+  },
+  props: {
+    idName: {
+      type: String as PropType<string>,
+      default: 'vsp-container'
+    },
+    hideCloseBtn: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    noOverlay: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    noClose: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    side: {
+      type: String as PropType<string>,
+      validator: (value: string) => ['top', 'right', 'bottom', 'left'].includes(value),
+      default: 'right'
+    },
+    rerender: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    zIndex: {
+      type: [Number, String] as PropType<number | 'auto' | undefined>,
+      default: 'auto'
+    },
+    width: {
+      type: String as PropType<string>,
+      default: 'auto'
+    },
+    height: {
+      type: String as PropType<string>,
+      default: 'auto'
+    },
+    lockScroll: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    lockScrollHtml: {
+      type: Boolean as PropType<boolean>,
+      default: true
+    },
+    modelValue: {
+      type: Boolean as PropType<boolean>,
+      default: false,
+      required: true
+    },
+    overlayColor: {
+      type: String as PropType<string>,
+      default: 'black'
+    },
+    overlayOpacity: {
+      type: Number as PropType<number>,
+      default: 0.5
+    },
+    overlayDuration: {
+      type: Number as PropType<number>,
+      default: 500
+    },
+    panelColor: {
+      type: String as PropType<string>,
+      default: 'white'
+    },
+    panelDuration: {
+      type: Number as PropType<number>,
+      default: 300
+    },
+    transitionName: {
+      type: String as PropType<string | undefined>,
+      default: undefined
+    },
+    headerClass: {
+      type: String as PropType<string>,
+      default: ''
+    },
+    bodyClass: {
+      type: String as PropType<string>,
+      default: ''
+    },
+    footerClass: {
+      type: String as PropType<string>,
+      default: ''
+    }
+  },
+  emits: ['update:modelValue', 'closed', 'opened'],
+  setup(props, { emit, attrs }) {
+    let teleportContainer = undefined as HTMLDivElement | undefined
+    const panel = ref<HTMLElement | null>(null)
+    const overlay = ref<HTMLElement | null>(null)
+    const footer = ref<HTMLElement | null>(null)
+    const header = ref<HTMLElement | null>(null)
+    const body = ref<HTMLElement | null>(null)
+    const footerHeight = ref(0)
+    const bodyScrollHeight = ref(0)
+    const headerHeight = ref(0)
+    const panelHeight = ref(0)
+    const windowHeight = ref(0)
+    const zIndex = ref<number>()
+    const isBodyAlreadyLocked = ref(false)
+
+    const calculateRightSize = async () => {
+      if (window?.innerHeight > 0) windowHeight.value = window.innerHeight
+
+      footerHeight.value = footer.value ? footer.value.clientHeight : 0
+      headerHeight.value = header.value ? header.value.clientHeight : 0
+      bodyScrollHeight.value = body.value ? body.value.scrollHeight : 0
+      panelHeight.value = panel.value ? panel.value.clientHeight : 0
+    }
+
+    const closePanel = () => emit('update:modelValue', false)
+
+    const lockUnlockBodyScroll = (elem: HTMLElement, lock: boolean) => {
+      if (lock) {
+        setTimeout(() => {
+          disableBodyScroll(elem, { reserveScrollBarGap: true })
+          if (props.lockScrollHtml) document.documentElement.style.overflow = 'hidden'
+        }, 0)
+        return
+      }
+
+      enableBodyScroll(elem)
+      if (props.lockScrollHtml) document.documentElement.style.removeProperty('overflow')
+    }
+
+    const getMaxZIndex = () =>
+      Math.max(
+        ...Array.from(document.querySelectorAll('body *'), (el) =>
+          parseFloat(window.getComputedStyle(el).zIndex)
+        ).filter((zIndex) => !Number.isNaN(zIndex)),
+        0
+      )
+
+    onMounted(() => {
+      zIndex.value = props.zIndex === 'auto' ? getMaxZIndex() : props.zIndex
+    })
+
+    onBeforeMount(() => {
+      const alreadyCreatedTarget = document.getElementById(props.idName)
+      if (alreadyCreatedTarget) return
+      teleportContainer = document.createElement('div')
+      teleportContainer.setAttribute('id', props.idName)
+      document.body.appendChild(teleportContainer)
+    })
+
+    onBeforeUnmount(() => {
+      const { modelValue, lockScroll } = props
+      if (lockScroll && panel.value && modelValue) lockUnlockBodyScroll(panel.value, false)
+      if (teleportContainer) document.body.removeChild(teleportContainer)
+      window.removeEventListener('resize', calculateRightSize)
+    })
+
+    watch(
+      () => [header.value, footer.value, props.height, props.width, props.side, props.modelValue],
+      () => {
+        nextTick(() => calculateRightSize())
+      }
+    )
+
+    watch(
+      () => [props.modelValue, panel.value],
+      (newP, oldP) => {
+        const wasShown = oldP ? (oldP[0] as boolean) : false
+        const [isShown, panelEl] = newP as [boolean, HTMLElement | null]
+        const isOpening = isShown
+        const isClosing = wasShown && !isShown
+        if (!panelEl) return
+        if (isOpening) isBodyAlreadyLocked.value = !!document.body.style.overflow
+
+        if (isShown) {
+          if (props.lockScroll) lockUnlockBodyScroll(panelEl, true)
+          calculateRightSize()
+          window.addEventListener('resize', calculateRightSize)
+          return
+        }
+
+        if (!props.lockScroll || !isClosing || isBodyAlreadyLocked.value) return
+
+        setTimeout(() => {
+          if (panelEl) lockUnlockBodyScroll(panelEl, false)
+        }, props.panelDuration)
+        window.removeEventListener('resize', calculateRightSize)
+      },
+      { immediate: true }
+    )
+
+    const bodyHeight = computed<number | undefined>(() => {
+      if (!panelHeight.value) return
+
+      const panelMaxHeight = bodyScrollHeight.value + headerHeight.value + footerHeight.value
+      let height = panelHeight.value - headerHeight.value - footerHeight.value
+
+      if (['top', 'bottom'].includes(props.side) && props.height === 'auto') {
+        height =
+          windowHeight.value >= panelMaxHeight
+            ? bodyScrollHeight.value
+            : windowHeight.value - headerHeight.value - footerHeight.value
+      }
+
+      return height
+    })
+
+    const overlayStyles = computed(() => ({
+      zIndex: zIndex.value,
+      animationDuration: `${props.overlayDuration}ms`,
+      '--overlay-opacity': props.overlayOpacity,
+      opacity: props.modelValue ? props.overlayOpacity : 0,
+      backgroundColor: props.overlayColor,
+      pointerEvents: !props.modelValue ? 'none' : 'all'
+    }))
+
+    const panelStyles = computed(() => ({
+      width: ['left', 'right'].includes(props.side) ? props.width : undefined,
+      maxWidth: '100%',
+
+      ...(['top', 'bottom'].includes(props.side)
+        ? {
+            // minHeight: props.height,
+            height: props.height,
+            maxHeight: '100%'
+          }
+        : {}),
+
+      zIndex: zIndex.value,
+      backgroundColor: props.panelColor,
+      animationDuration: `${props.panelDuration}ms`,
+
+      ...Object.assign({}, attrs.style)
+    }))
+
+    return {
+      body,
+      panel,
+      overlay,
+      overlayStyles,
+      header,
+      footer,
+      closePanel,
+      panelStyles,
+      bodyHeight
+    }
+  }
+})
+</script>
+
+<template>
+  <teleport :to="`#${idName}`">
+    <div class="vsp-wrapper" :class="[modelValue && 'vsp-wrapper--active']">
+      <Transition name="overlay">
+        <div
+          v-if="!noOverlay"
+          v-show="modelValue"
+          ref="overlay"
+          class="vsp-overlay"
+          @click="() => (noClose ? undefined : closePanel())"
+        />
+      </Transition>
+      <Transition
+        :name="transitionName || `slide-${side}`"
+        @after-enter="$emit('opened')"
+        @after-leave="$emit('closed')"
+      >
+        <div
+          v-if="rerender ? modelValue : true"
+          v-show="rerender ? true : modelValue"
+          ref="panel"
+          class="vsp"
+          :class="[`vsp--${side}-side`, $attrs.class]"
+          :style="panelStyles"
+        >
+          <div v-if="!!$slots.header" ref="header" :class="[headerClass, 'vsp__header']">
+            <slot name="header" :close="closePanel" />
+          </div>
+          <div ref="body" :class="[bodyClass, 'vsp__body']" :style="{ height: `${bodyHeight}px`}">
+            <slot name="default" :close="closePanel" />
+            <SidePanelCloseButton v-if="!hideCloseBtn" @close="closePanel" />
+          </div>
+          <div v-if="!!$slots.footer" ref="footer" :class="[footerClass, 'vsp__footer']">
+            <slot name="footer" />
+          </div>
+        </div>
+      </Transition>
+    </div>
+  </teleport>
+</template>
+
+<style lang="scss">
+.vsp-wrapper {
+  .vsp-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .vsp {
+    position: fixed;
+
+    &--right-side,
+    &--left-side {
+      top: 0;
+      height: 100%;
+    }
+
+    &--right-side {
+      right: 0;
+      left: unset;
+    }
+
+    &--left-side {
+      right: unset;
+      left: 0;
+    }
+
+    &--bottom-side,
+    &--top-side {
+      left: 0;
+      width: 100%;
+    }
+
+    &--bottom-side {
+      bottom: 0;
+    }
+
+    &--top-side {
+      top: 0;
+    }
+
+    &__header,
+    &__body {
+      overflow: visible;
+    }
+    &__footer {
+      overflow: auto;
+    }
+
+    &__body {
+      position: relative;
+    }
+  }
+
+  .overlay-enter-active,
+  .overlay-leave-active {
+    animation: overlay-transition;
+  }
+
+  .overlay-leave-active {
+    animation-direction: reverse;
+  }
+
+  .slide-right-enter-active,
+  .slide-right-leave-active {
+    animation: slide-right;
+  }
+
+  .slide-right-leave-active {
+    animation-direction: reverse;
+  }
+
+  .slide-left-enter-active,
+  .slide-left-leave-active {
+    animation: slide-left;
+  }
+
+  .slide-left-leave-active {
+    animation-direction: reverse;
+  }
+
+  .slide-top-enter-active,
+  .slide-top-leave-active {
+    animation: slide-left;
+  }
+
+  .slide-top-leave-active {
+    animation-direction: reverse;
+  }
+
+  .slide-top-enter-active,
+  .slide-top-leave-active {
+    animation: slide-top;
+  }
+
+  .slide-top-leave-active {
+    animation-direction: reverse;
+  }
+
+  .slide-bottom-enter-active,
+  .slide-bottom-leave-active {
+    animation: slide-bottom;
+  }
+
+  .slide-bottom-leave-active {
+    animation-direction: reverse;
+  }
+
+  @keyframes slide-right {
+    0% {
+      transform: translateX(100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-left {
+    0% {
+      transform: translateX(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-bottom {
+    0% {
+      transform: translateY(100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-top {
+    0% {
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-top {
+    0% {
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes overlay-transition {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: var(--overlay-opacity);
+    }
+  }
+}
+</style>
