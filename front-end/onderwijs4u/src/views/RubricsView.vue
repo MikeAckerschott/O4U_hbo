53777b581f0db520f1402d7816cf8d7c0f4e7b58
Mike Ackerschott <MTJK.Ackerschott@student.han.nl>
2024-10-04 12:52:21 +0200

    Filter criteria by workproces on frontend using updated dummydata

--- front-end/onderwijs4u/src/views/RubricsView.vue
+++ front-end/onderwijs4u/src/views/RubricsView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container-fluid mt-5">
     <div class="row mb-3">
-      <div class="col-md-3">
+      <div class="col-md-2">
         <label for="faseFilter" class="form-label">Filter by Fase:</label>
         <select id="faseFilter" v-model="selectedFase" @change="filterData" class="form-select">
           <option value="">All</option>
           <option v-for="fase in uniqueFases" :key="fase" :value="fase">{{ fase }}</option>
         </select>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-2">
         <label for="beoordelingFilter" class="form-label">Filter by Beoordeling:</label>
         <select id="beoordelingFilter" v-model="selectedBeoordeling" @change="filterData" class="form-select">
           <option value="">All</option>
-          <option v-for="beoordeling in uniqueBeoordelingen" :key="beoordeling" :value="beoordeling">{{ beoordeling }}</option>
+          <option v-for="beoordeling in uniqueBeoordelingen" :key="beoordeling" :value="beoordeling">{{ beoordeling }}
+          </option>
+        </select>
+      </div>
+      <div class="col-md-2">
+        <label for="WerkprocesFilter" class="form-label">Selected Workprocess:</label>
+        <select id="WerkprocesFilter" v-model="currentWerkProces" @change="filterData" class="form-select">
+          <!-- <option v-for="beoordeling in uniqueBeoordelingen" :key="beoordeling" :value="beoordeling">{{ beoordeling }}          </option> -->
+          <option v-for="werkproces, index in data[0].werkproces" :key="werkproces" :value="index">{{ werkproces.description }}</option>
         </select>
       </div>
       <div class="col-md-6 text-end">
@@ -41,33 +49,33 @@
         <div class="table-responsive">
           <table class="table table-striped table-hover table-bordered w-100">
             <thead class="bg-light-blue text-white">
-            <tr>
-              <th @click="sort('beoordelingscriteria')" class="cursor-pointer">
-                Rubrics
-                <i :class="getSortIcon('beoordelingscriteria')"></i>
-              </th>
-              <th @click="sort('fase')" class="cursor-pointer">
-                Fase
-                <i :class="getSortIcon('fase')"></i>
-              </th>
-              <th @click="sort('beoordeling')" class="cursor-pointer">
-                Beoordeling
-                <i :class="getSortIcon('beoordeling')"></i>
-              </th>
-            </tr>
+              <tr>
+                <th @click="sort('beoordelingscriteria')" class="cursor-pointer">
+                  Criteria
+                  <i :class="getSortIcon('beoordelingscriteria')"></i>
+                </th>
+                <th @click="sort('fase')" class="cursor-pointer">
+                  Fase
+                  <i :class="getSortIcon('fase')"></i>
+                </th>
+                <th @click="sort('beoordeling')" class="cursor-pointer">
+                  Beoordeling
+                  <i :class="getSortIcon('beoordeling')"></i>
+                </th>
+              </tr>
             </thead>
             <tbody>
-            <tr v-for="item in paginatedData" :key="item.id" :class="getRowClass(item.beoordeling)">
-              <td>
-                <RouterLink class="nav-link" :to="`/rubric/${item.beoordelingscriteria}`">
-                   {{ item.beoordelingscriteria }}
-                </RouterLink>
-              </td>
-              <td>{{ item.fase }}</td>
-              <td>
-                <span :class="getBadgeClass(item.beoordeling)">{{ item.beoordeling }}</span>
-              </td>
-            </tr>
+              <tr v-for="item in paginatedData" :key="item.id" :class="getRowClass(item.beoordeling)">
+                <td>
+                  <RouterLink class="nav-link" :to="`/rubric/${item.beoordelingscriteria}`">
+                    {{ item.beoordelingscriteria }}
+                  </RouterLink>
+                </td>
+                <td>{{ item.fase }}</td>
+                <td>
+                  <span :class="getBadgeClass(item.beoordeling)">{{ item.beoordeling }}</span>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
@@ -92,48 +100,65 @@
 <script setup>
 import { ref, computed } from 'vue';
 
+
+
 const data = ref([
-  {id: 1, beoordelingscriteria: '25192', fase: 'Ontwikkeling', beoordeling: 'Goed'},
-  {id: 2, beoordelingscriteria: '25194', fase: 'Uitvoerings', beoordeling: 'Voldoende'},
-  {id: 3, beoordelingscriteria: '25191', fase: 'Afronding', beoordeling: 'Onvoldoende'},
-  {id: 4, beoordelingscriteria: '25193', fase: 'Voorbereiding', beoordeling: 'Onvoldoende'},
-  {id: 5, beoordelingscriteria: '25195', fase: 'Ontwikkeling', beoordeling: 'Goed'},
-  {id: 6, beoordelingscriteria: '25196', fase: 'Uitvoerings', beoordeling: 'Voldoende'},
-  {id: 7, beoordelingscriteria: '25197', fase: 'Afronding', beoordeling: 'Goed'},
-  {id: 8, beoordelingscriteria: '25198', fase: 'Voorbereiding', beoordeling: 'Onvoldoende'},
-  {id: 9, beoordelingscriteria: '25199', fase: 'Ontwikkeling', beoordeling: 'Voldoende'},
-  {id: 10, beoordelingscriteria: '25200', fase: 'Uitvoerings', beoordeling: 'Goed'},
-  {id: 11, beoordelingscriteria: '25201', fase: 'Afronding', beoordeling: 'Onvoldoende'},
-  {id: 12, beoordelingscriteria: '25202', fase: 'Voorbereiding', beoordeling: 'Voldoende'},
-  {id: 13, beoordelingscriteria: '25203', fase: 'Ontwikkeling', beoordeling: 'Goed'},
-  {id: 14, beoordelingscriteria: '25204', fase: 'Uitvoerings', beoordeling: 'Onvoldoende'},
-  {id: 15, beoordelingscriteria: '25205', fase: 'Afronding', beoordeling: 'Voldoende'},
-  {id: 16, beoordelingscriteria: '25206', fase: 'Voorbereiding', beoordeling: 'Goed'},
-  {id: 17, beoordelingscriteria: '25207', fase: 'Ontwikkeling', beoordeling: 'Onvoldoende'},
-  {id: 18, beoordelingscriteria: '25208', fase: 'Uitvoerings', beoordeling: 'Voldoende'},
-  {id: 19, beoordelingscriteria: '25209', fase: 'Afronding', beoordeling: 'Goed'},
-  {id: 20, beoordelingscriteria: '25210', fase: 'Voorbereiding', beoordeling: 'Onvoldoende'},
-  {id: 21, beoordelingscriteria: '25211', fase: 'Ontwikkeling', beoordeling: 'Voldoende'},
-  {id: 22, beoordelingscriteria: '25212', fase: 'Uitvoerings', beoordeling: 'Goed'},
-  {id: 23, beoordelingscriteria: '25213', fase: 'Afronding', beoordeling: 'Onvoldoende'},
-  {id: 24, beoordelingscriteria: '25214', fase: 'Voorbereiding', beoordeling: 'Voldoende'},
-  {id: 25, beoordelingscriteria: '25215', fase: 'Ontwikkeling', beoordeling: 'Goed'},
-  {id: 26, beoordelingscriteria: '25216', fase: 'Uitvoerings', beoordeling: 'Onvoldoende'},
-  {id: 27, beoordelingscriteria: '25217', fase: 'Afronding', beoordeling: 'Voldoende'},
-  {id: 28, beoordelingscriteria: '25218', fase: 'Voorbereiding', beoordeling: 'Goed'},
-  {id: 29, beoordelingscriteria: '25219', fase: 'Ontwikkeling', beoordeling: 'Onvoldoende'},
-  {id: 30, beoordelingscriteria: '25220', fase: 'Uitvoerings', beoordeling: 'Voldoende'},
-  {id: 31, beoordelingscriteria: '25221', fase: 'Afronding', beoordeling: 'Goed'},
-  {id: 32, beoordelingscriteria: '25222', fase: 'Voorbereiding', beoordeling: 'Onvoldoende'},
-  {id: 33, beoordelingscriteria: '25223', fase: 'Ontwikkeling', beoordeling: 'Voldoende'},
-  {id: 34, beoordelingscriteria: '25224', fase: 'Uitvoerings', beoordeling: 'Goed'},
-  {id: 35, beoordelingscriteria: '25225', fase: 'Afronding', beoordeling: 'Onvoldoende'},
-  {id: 36, beoordelingscriteria: '25226', fase: 'Voorbereiding', beoordeling: 'Voldoende'},
-  {id: 37, beoordelingscriteria: '25227', fase: 'Ontwikkeling', beoordeling: 'Goed'},
-  {id: 38, beoordelingscriteria: '25228', fase: 'Uitvoerings', beoordeling: 'Onvoldoende'},
-  {id: 39, beoordelingscriteria: '25229', fase: 'Afronding', beoordeling: 'Voldoende'},
-  {id: 40, beoordelingscriteria: '25230', fase: 'Voorbereiding', beoordeling: 'Goed'},
+  {
+    werkproces: [
+      {
+        description: "Werkproces 1 Description",
+        criteria: [
+          { id: 1, beoordelingscriteria: '25192', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 2, beoordelingscriteria: '25194', fase: 'Uitvoerings', beoordeling: 'Voldoende' },
+          { id: 3, beoordelingscriteria: '25191', fase: 'Afronding', beoordeling: 'Onvoldoende' },
+          { id: 4, beoordelingscriteria: '25193', fase: 'Voorbereiding', beoordeling: 'Onvoldoende' },
+          { id: 5, beoordelingscriteria: '25195', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 6, beoordelingscriteria: '25196', fase: 'Uitvoerings', beoordeling: 'Voldoende' },
+          { id: 7, beoordelingscriteria: '25197', fase: 'Afronding', beoordeling: 'Goed' },
+          { id: 8, beoordelingscriteria: '25198', fase: 'Voorbereiding', beoordeling: 'Onvoldoende' },
+          { id: 9, beoordelingscriteria: '25199', fase: 'Ontwikkeling', beoordeling: 'Voldoende' },
+          { id: 10, beoordelingscriteria: '25200', fase: 'Uitvoerings', beoordeling: 'Goed' },
+          { id: 11, beoordelingscriteria: '25201', fase: 'Afronding', beoordeling: 'Onvoldoende' },
+          { id: 12, beoordelingscriteria: '25202', fase: 'Voorbereiding', beoordeling: 'Voldoende' },
+          { id: 13, beoordelingscriteria: '25203', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 14, beoordelingscriteria: '25204', fase: 'Uitvoerings', beoordeling: 'Onvoldoende' },
+          { id: 15, beoordelingscriteria: '25205', fase: 'Afronding', beoordeling: 'Voldoende' },
+          { id: 16, beoordelingscriteria: '25206', fase: 'Voorbereiding', beoordeling: 'Goed' },
+          { id: 17, beoordelingscriteria: '25207', fase: 'Ontwikkeling', beoordeling: 'Onvoldoende' },
+          { id: 18, beoordelingscriteria: '25208', fase: 'Uitvoerings', beoordeling: 'Voldoende' },
+          { id: 19, beoordelingscriteria: '25209', fase: 'Afronding', beoordeling: 'Goed' },
+          { id: 20, beoordelingscriteria: '25210', fase: 'Voorbereiding', beoordeling: 'Onvoldoende' }
+        ]
+      },
+      {
+        description: "Werkproces 2 Description",
+        criteria: [
+          { id: 1, beoordelingscriteria: '26192', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 2, beoordelingscriteria: '26194', fase: 'Uitvoerings', beoordeling: 'Goed' },
+          { id: 3, beoordelingscriteria: '26191', fase: 'Afronding', beoordeling: 'Goed' },
+          { id: 4, beoordelingscriteria: '26193', fase: 'Voorbereiding', beoordeling: 'Goed' },
+          { id: 5, beoordelingscriteria: '26195', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 6, beoordelingscriteria: '26196', fase: 'Uitvoerings', beoordeling: 'Goed' },
+          { id: 7, beoordelingscriteria: '26197', fase: 'Afronding', beoordeling: 'Goed' },
+          { id: 8, beoordelingscriteria: '26198', fase: 'Voorbereiding', beoordeling: 'Onvoldoende' },
+          { id: 9, beoordelingscriteria: '26199', fase: 'Ontwikkeling', beoordeling: 'Voldoende' },
+          { id: 10, beoordelingscriteria: '26200', fase: 'Uitvoerings', beoordeling: 'Goed' },
+          { id: 11, beoordelingscriteria: '26201', fase: 'Afronding', beoordeling: 'Onvoldoende' },
+          { id: 12, beoordelingscriteria: '26202', fase: 'Voorbereiding', beoordeling: 'Voldoende' },
+          { id: 13, beoordelingscriteria: '26203', fase: 'Ontwikkeling', beoordeling: 'Goed' },
+          { id: 14, beoordelingscriteria: '26204', fase: 'Uitvoerings', beoordeling: 'Onvoldoende' },
+          { id: 15, beoordelingscriteria: '26205', fase: 'Afronding', beoordeling: 'Voldoende' },
+          { id: 16, beoordelingscriteria: '26206', fase: 'Voorbereiding', beoordeling: 'Goed' },
+          { id: 17, beoordelingscriteria: '26207', fase: 'Ontwikkeling', beoordeling: 'Onvoldoende' },
+          { id: 18, beoordelingscriteria: '26208', fase: 'Uitvoerings', beoordeling: 'Voldoende' },
+          { id: 19, beoordelingscriteria: '26209', fase: 'Afronding', beoordeling: 'Goed' },
+          { id: 20, beoordelingscriteria: '26210', fase: 'Voorbereiding', beoordeling: 'Onvoldoende' }
+        ]
+      }
+    ]
+  }
 ]);
+
 
 const sortKey = ref('');
 const sortOrder = ref('asc');
@@ -141,6 +166,8 @@ const selectedFase = ref('');
 const selectedBeoordeling = ref('');
 const currentPage = ref(1);
 const itemsPerPage = 10;
+
+
 
 const sort = (key) => {
   if (sortKey.value === key) {
@@ -157,7 +184,8 @@ const getSortIcon = (key) => {
 };
 
 const sortedData = computed(() => {
-  const sorted = [...data.value];
+  // console.log(data.value[0].werkproces);
+  const sorted = [...data.value[0].werkproces[currentWerkProces.value].criteria];
   if (sortKey.value) {
     sorted.sort((a, b) => {
       const aValue = a[sortKey.value];
@@ -177,34 +205,6 @@ const sortedData = computed(() => {
   return sorted;
 });
 
-const uniqueFases = computed(() => {
-  const fases = data.value.map(item => item.fase);
-  return [...new Set(fases)];
-});
-
-const uniqueBeoordelingen = computed(() => {
-  const beoordelingen = data.value.map(item => item.beoordeling);
-  return [...new Set(beoordelingen)];
-});
-
-const filteredData = computed(() => {
-  let filtered = sortedData.value;
-  if (selectedFase.value) {
-    filtered = filtered.filter(item => item.fase === selectedFase.value);
-  }
-  if (selectedBeoordeling.value) {
-    filtered = filtered.filter(item => item.beoordeling === selectedBeoordeling.value);
-  }
-  return filtered;
-});
-
-const totalPages = computed(() => Math.ceil(filteredData.value.length / itemsPerPage));
-
-const paginatedData = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  return filteredData.value.slice(start, end);
-});
 
 const filterData = () => {
   currentPage.value = 1;
@@ -238,25 +238,63 @@ const getBadgeClass = (beoordeling) => {
   }
 };
 
+// Make currentWerkProces reactive
+const currentWerkProces = ref(0); // Wrap currentWerkProces in a ref
+
+const incrementWerkProces = () => {
+  currentWerkProces.value += 1;
+  console.log(currentWerkProces.value);
+};
+
+const uniqueFases = computed(() => {
+  console.log(data.value[0].werkproces[currentWerkProces.value].criteria)
+  const fases = data.value[0].werkproces[currentWerkProces.value].criteria.map(item => item.fase);
+  return [...new Set(fases)];
+});
+
+const uniqueBeoordelingen = computed(() => {
+  const beoordelingen = data.value[0].werkproces[currentWerkProces.value].criteria.map(item => item.beoordeling);
+  return [...new Set(beoordelingen)];
+});
+
+const filteredData = computed(() => {
+  let filtered = sortedData.value;
+  if (selectedFase.value) {
+    filtered = filtered.filter(item => item.fase === selectedFase.value);
+  }
+  if (selectedBeoordeling.value) {
+    filtered = filtered.filter(item => item.beoordeling === selectedBeoordeling.value);
+  }
+  return filtered;
+});
+
+const totalPages = computed(() => Math.ceil(filteredData.value.length / itemsPerPage));
+
+const paginatedData = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return filteredData.value.slice(start, end);
+});
+
 // Calculate percentages
 const percentageCompleted = computed(() => {
-  const completed = data.value.filter(item => item.beoordeling === 'Goed' || item.beoordeling === 'Voldoende').length;
-  return ((completed / data.value.length) * 100).toFixed(2);
+  const completed = data.value[0].werkproces[currentWerkProces.value].criteria.filter(item => item.beoordeling === 'Goed' || item.beoordeling === 'Voldoende').length;
+  return ((completed / data.value[0].werkproces[currentWerkProces.value].criteria.length) * 100).toFixed(2);
 });
 
 const percentageVoldoende = computed(() => {
-  const voldoende = data.value.filter(item => item.beoordeling === 'Voldoende').length;
-  return ((voldoende / data.value.length) * 100).toFixed(2);
+  const voldoende = data.value[0].werkproces[currentWerkProces.value].criteria.filter(item => item.beoordeling === 'Voldoende').length;
+  return ((voldoende / data.value[0].werkproces[currentWerkProces.value].criteria.length) * 100).toFixed(2);
 });
 
 const percentageGoed = computed(() => {
-  const goed = data.value.filter(item => item.beoordeling === 'Goed').length;
-  return ((goed / data.value.length) * 100).toFixed(2);
+  const goed = data.value[0].werkproces[currentWerkProces.value].criteria.filter(item => item.beoordeling === 'Goed').length;
+  return ((goed / data.value[0].werkproces[currentWerkProces.value].criteria.length) * 100).toFixed(2);
 });
 
 const percentageOnvoldoende = computed(() => {
-  const onvoldoende = data.value.filter(item => item.beoordeling === 'Onvoldoende').length;
-  return ((onvoldoende / data.value.length) * 100).toFixed(2);
+  const onvoldoende = data.value[0].werkproces[currentWerkProces.value].criteria.filter(item => item.beoordeling === 'Onvoldoende').length;
+  return ((onvoldoende / data.value[0].werkproces[currentWerkProces.value].criteria.length) * 100).toFixed(2);
 });
 
 // Donut chart styles
